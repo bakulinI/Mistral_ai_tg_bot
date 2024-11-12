0de8a37697297777c25bd3718be13f035a3eccc3
Ilya Bakulin <ilabakulin77@gmail.com>
2024-11-12 21:39:42 +0300

    first commit

--- app/user.py
+++ app/user.py
@@ -1,10 +1,12 @@
 from aiogram import Router, F
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart, Command
+from aiogram.fsm.context import FSMContext
 
 from app.database.requests import set_user
 # from middlewares import BaseMiddleware
-
+from app.generator import generate
+from app.states import Work
 user = Router()
 
 # user.message.middleware(BaseMiddleware())
@@ -12,4 +14,16 @@ user = Router()
 @user.message(CommandStart())
 async def cmd_start(message: Message):
     await set_user(message.from_user.id)
-    await message.answer('Добро пожаловать в бот!')
+    await message.answer('Добро пожаловать в бот! Введите ваш запрос.')
+
+@user.message(Work.process)
+async def stop(message: Message):
+    await message.answer('Подождите, ваше предыдущее сообщение генерируется...')
+
+@user.message()
+async def ai(message: Message, state: FSMContext):
+    await state.set_state(Work.process)
+    await message.answer('Подождите, ваше сообщение генерируется...')
+    res = await generate(message.text)
+    await message.answer(res.choices[0].message.content)
+    await state.clear()
--- config.py
+++ config.py
@@ -1,5 +1,4 @@
-TOKEN=''
+TOKEN='8037088294:AAGBHyrjAi4UOsfeLLHBGr10NfqLAxYf44Q'
 DB_URL='sqlite+aiosqlite:///db.sqlite3'
 #DB_URL='postgresql+asyncpg://user:password@host:port/dbname[?key=value&key=value...]'
-
-# Внимание! Добавьте config.py в файл .gitignore!
+AI_TOKEN = '88VYdSn2hSlXvscGL6W55Z3pdAOQpj0s'
